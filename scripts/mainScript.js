5397523f30b1199552115b5bd9346aff7c315815
OrangeC7 <lilwormser@gmail.com>
2018-12-04 14:41:08 -0600

    Attempt to force p5 to draw in the "test-script" container

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -1,18 +1,22 @@
-function setup() {
-	createCanvas(400, 400)
-}
+function sketch(p) {
+	let p.setup = function() {
+		createCanvas(400, 400)
+	}
 
-function draw() {
-	background(220)
-    
-	bgOnMouse(color(0, 0, 0))
-}
+	let p.draw = function() {
+		background(220)
+		
+		bgOnMouse(color(0, 0, 0))
+	}
 
-function bgOnMouse(color) {
-// Set the background to black when the mouse is pressed in the canvas
-	if (mouseIsPressed) {
-		if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= width) {
-			background(color)
+	let p.bgOnMouse = function(color) {
+	// Set the background to black when the mouse is pressed in the canvas
+		if (mouseIsPressed) {
+			if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= width) {
+				background(color)
+			}
 		}
 	}
 }
+
+new p5(sketch, 'test-script');
